ef8536503f2474c4c67c0995cfc50478ee54a3b8
Jeremiah <jeremiah.bergkvist@gmail.com>
2017-09-01 13:58:30 -0400

    Merged netstat output into main script.

--- enumeration_html.bat
+++ enumeration_html.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enableDelayedExpansion
 
 echo ^<html^> > survey.html
 echo ^<head^> >> survey.html
@@ -16,7 +17,44 @@ echo ^</html^> >> survey.html
 
 echo ^<a name="netstat"^>^</a^> >> survey.html
 echo ^<p^>^<a href="#top"^>Return to top.^</a^>^</p^> >> survey.html
-call netstat_html.bat >> survey.html
+
+:: Format netstat output into html table
+set light=TRUE
+
+echo ^<html^> >> survey.html
+echo ^<H3^>Results from netstat -ano^</H3^> >> survey.html
+echo ^<table border="1"\^> >> survey.html
+echo ^<tr style="background-color:#a0a0ff;font:10pt Tahoma;font-weight:bold;" align="left"\^> >> survey.html
+echo ^<td^>Proto^</td^> >> survey.html
+echo ^<td^>Local Address^</td^> >> survey.html
+echo ^<td^>Foreign Address^</td^> >> survey.html
+echo ^<td^>State^</td^> >> survey.html
+echo ^<td^>PID^</td^> >> survey.html
+echo ^</tr^> >> survey.html
+
+for /F "tokens=*" %%a in ( 'netstat -ano' ) do (
+	call :flipvar
+	if NOT "%%a" == "Active Connections" ( 
+		if NOT "%%a" == "Proto  Local Address          Foreign Address        State           PID" (
+			for %%b in ( %%a ) do ( echo ^<td^>%%b^</td^> >> survey.html )
+			echo ^</tr^> >> survey.html
+		)
+	)
+)
+echo ^</table^> >> survey.html
+echo ^</html^> >> survey.html
+goto END
+
+:flipvar
+if "%light%" == "TRUE" (
+	echo ^<tr style="background-color:#e0f0f0;font:10pt Tahoma;"^> >> survey.html
+	set light=FALSE
+) else (
+	echo ^<tr style="background-color:#f0f0f0;font:10pt Tahoma;"^> >> survey.html
+	set light=TRUE
+)
+goto :EOF
+:END
 
 echo ^<a name="service"^>^</a^> >> survey.html
 echo ^<p^>^<a href="#top"^>Return to top.^</a^>^</p^> >> survey.html
